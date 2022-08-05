7cbfc26d01e5ac4337c17699bd3e32fe90da2c62
tim <timofey.evsigneev@gmail.com>
2022-08-05 15:55:51 +0300

    Start project with Spring Boot initialization and configuration for database

---
+++ CityBoard/src/main/java/com/CityBoard/CityBoardApplication.java
@@ -0,0 +1,13 @@
+package com.CityBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityBoardApplication.class, args);
+	}
+
+}
---
+++ CityBoard/src/test/java/com/CityBoard/CityBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CityBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
