d10c05c5a9a5e7a453a500f24b458ffe84ce8819
tim <timofey.evsigneev@gmail.com>
2022-08-12 15:21:16 +0300

    Add registration service and basic Users controller and repository

---
+++ CityBoard/src/main/java/com/CityBoard/controllers/UserController.java
@@ -0,0 +1,67 @@
+package com.CityBoard.controllers;
+
+import com.CityBoard.DTO.UserCredentialsDTO;
+import com.CityBoard.models.Users;
+import com.CityBoard.services.CustomUserDetailsService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class UserController {
+    private final CustomUserDetailsService userDetailsService;
+
+    public UserController(CustomUserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @GetMapping("/registration")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("userCredentialsDTO", new UserCredentialsDTO());
+        return "registration";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(UserCredentialsDTO userCredentials, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("registrationForm", userCredentials);
+            return "registration";
+        }
+        try {
+            userDetailsService.registerUser(userCredentials);
+        }
+        catch (Exception exception) {
+            bindingResult.rejectValue("username", "userCredentialsDTO.username",
+                    "This username already exists");
+            model.addAttribute("registrationForm", userCredentials);
+            return "registration";
+        }
+        return "redirect:/login";
+    }
+
+    @GetMapping("/admin/users")
+    public String showUserList(Model model) {
+        return "index";
+    }
+
+    @GetMapping("/admin/users/{id}")
+    public String showUpdateUserForm(@PathVariable("id") Long id, Model model) {
+        return "update-user";
+    }
+
+    @GetMapping("/profile")
+    public String showUserProfile(Users user, Model model) {
+        return "profile";
+    }
+
+    @GetMapping("/my-adverts")
+    public String showUserAdverts(Users user, Model model) {
+        return "my-adverts";
+    }
+
+    @GetMapping("/my-requests")
+    public String showUserRequests(Users user, Model model) {
+        return "my-requests";
+    }
+}
---
+++ CityBoard/src/main/java/com/CityBoard/services/CustomUserDetailsService.java
@@ -0,0 +1,54 @@
+package com.CityBoard.services;
+
+import com.CityBoard.DTO.UserCredentialsDTO;
+import com.CityBoard.models.Roles;
+import com.CityBoard.models.UserStatus;
+import com.CityBoard.models.Users;
+import com.CityBoard.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    public CustomUserDetailsService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username);
+    }
+
+    public boolean userExists(String username) {
+        return userRepository.findByUsername(username) != null ? true : false;
+    }
+
+    public void registerUser(UserCredentialsDTO userCredentials) throws Exception {
+        if (userExists(userCredentials.getUsername()))
+        {
+            throw new Exception("User with this username already exists");
+        }
+        System.out.println("We are here!\n");
+        Set<Roles> userRoles = new HashSet<Roles>();
+        userRoles.add(Roles.ROLE_USER);
+        Users user = Users.builder()
+                .username(userCredentials.getUsername())
+                .password(passwordEncoder.encode(userCredentials.getPassword()))
+                .created_at(new Timestamp(System.currentTimeMillis()))
+                .password_expired(false)
+                .status(UserStatus.LOGGED_OFF)
+                .roles(userRoles)
+                .build();
+        userRepository.save(user);
+    }
+}
