1059fa82b4fbd3b24666fb8237cbb6308eae9278
tim <timofey.evsigneev@gmail.com>
2022-08-13 00:06:14 +0300

    Find circular dependency error

--- CityBoard/src/main/java/com/CityBoard/services/CustomUserDetailsService.java
+++ CityBoard/src/main/java/com/CityBoard/services/CustomUserDetailsService.java
@@ -5,6 +5,7 @@ import com.CityBoard.models.Roles;
 import com.CityBoard.models.UserStatus;
 import com.CityBoard.models.Users;
 import com.CityBoard.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
