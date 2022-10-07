f55e404d3f14533b361877c0c59dffa4a4bc368d
tim <timofey.evsigneev@gmail.com>
2022-10-08 00:14:06 +0300

    Populate database with Faker

--- CityBoard/controllers/src/main/java/com.CityBoard.controllers/AdminController.java
+++ CityBoard/controllers/src/main/java/com.CityBoard.controllers/AdminController.java
@@ -1,12 +1,24 @@
 package com.CityBoard.controllers;
 
+import com.CityBoard.services.Populator;
 import com.CityBoard.ui.AdminUI;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class AdminController {
     private final AdminUI ui;
+    private final Populator populator;
 
-    public AdminController(AdminUI ui) {
+    public AdminController(AdminUI ui, Populator populator) {
         this.ui = ui;
+        this.populator = populator;
     }
-    
+
+    @GetMapping("/populate")
+    public String populateDatabase()
+    {
+        populator.populateDatabase();
+        return "redirect:/index";
+    }
 }
