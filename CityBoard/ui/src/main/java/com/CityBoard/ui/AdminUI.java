079171580ceadffda26590af6f4ccab585c7ae7c
tim <timofey.evsigneev@gmail.com>
2022-10-18 15:53:04 +0300

    Fix cascade deletion bug with adverts

--- CityBoard/ui/src/main/java/com/CityBoard/ui/AdminUI.java
+++ CityBoard/ui/src/main/java/com/CityBoard/ui/AdminUI.java
@@ -1,9 +1,11 @@
 package com.CityBoard.ui;
 
 import com.CityBoard.models.Adverts;
+import com.CityBoard.models.Requests;
 import com.CityBoard.models.Users;
 import com.CityBoard.models.enums.Roles;
 import com.CityBoard.services.AdvertsService;
+import com.CityBoard.services.RequestsService;
 import com.CityBoard.services.UsersService;
 import com.CityBoard.ui.operations.AdminOperations;
 import org.springframework.stereotype.Service;
@@ -13,10 +15,12 @@ import java.util.List;
 @Service
 public class AdminUI implements AdminOperations {
     private final AdvertsService advertsService;
+    private final RequestsService requestsService;
     private final UsersService usersService;
 
-    public AdminUI(AdvertsService advertsService, UsersService usersService) {
+    public AdminUI(AdvertsService advertsService, RequestsService requestsService, UsersService usersService) {
         this.advertsService = advertsService;
+        this.requestsService = requestsService;
         this.usersService = usersService;
     }
 
@@ -29,6 +33,10 @@ public class AdminUI implements AdminOperations {
     public void deleteAdvertForever(Long advertId) {
         Adverts advert = advertsService.getAdvertById(advertId);
         if (advert != null) {
+            List<Requests> requests = advert.getRequests();
+            for (Requests request : requests) {
+                requestsService.delete(request);
+            }
             advertsService.deleteAdvertPermanently(advert);
         }
     }
