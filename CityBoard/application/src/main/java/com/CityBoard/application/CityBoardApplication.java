72b7130610bdc4fcbf74e558ae379e56ddcf4978
tim <timofey.evsigneev@gmail.com>
2022-11-06 23:47:31 +0300

    Externalize port setup, branch the old version away

--- CityBoard/application/src/main/java/com/CityBoard/application/CityBoardApplication.java
+++ CityBoard/application/src/main/java/com/CityBoard/application/CityBoardApplication.java
@@ -3,15 +3,18 @@ package com.CityBoard.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories(basePackages = "com.CityBoard.repositories")
-@EntityScan(basePackages = "com.CityBoard.models")
-@ComponentScan(basePackages = "com.CityBoard")
-@SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.CityBoard.postgresql.repository")
+@EntityScan(basePackages = "com.CityBoard.postgresql.dto")
+@SpringBootApplication(scanBasePackages = {"com.CityBoard.application",
+                                           "com.CityBoard.models",
+                                           "com.CityBoard.controllers",
+                                           //"com.CityBoard.rest",
+                                           "com.CityBoard.services",
+                                           "com.CityBoard.ui"})
 public class CityBoardApplication {
-public static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(CityBoardApplication.class, args);
     }
 }
